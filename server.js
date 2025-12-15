efec97eae81cfb2a91d5f027c76d901e6e16b2ec
Maleska <alejandro.matias@hotmail.com>
2025-12-15 16:02:32 -0600

    servertime
    se agrego nueva funcion para el horario

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const twilio = require("twilio");
 const admin = require("firebase-admin");
+const hora = require("firebase-admin/database")
 
 // Inicializar Firebase Admin
 admin.initializeApp({
@@ -128,7 +129,7 @@ async function registrar(empleado, numero, tipo, extra = {}) {
     extra,
     fecha: Date.now(),
     dia:fechaHora.getFullYear()+'-'+ (fechaHora.getMonth() + 1) +'-'+ fechaHora.getDate(),
-    fechaHora: hour +':'+ min +':'+ sec
+    fechaHora: serverTimestamp //hour +':'+ min +':'+ sec
   });
 }
 
