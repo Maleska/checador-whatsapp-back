d6a0e18eb9087bab25e71a0e5ff00b89f5fe07f9
Maleska <alejandro.matias@hotmail.com>
2026-01-21 15:10:03 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.post("/webhook-whatsapp", async (req, res) => {
     const changes = entry?.changes?.[0];
     const value = changes?.value;
     const messageObj = value?.messages?.[0];
-    console.log("Mensaje recibido:", entry);
-    const text = entry;
+    console.log("Mensaje recibido:", messageObj.type);
+    const text = messageObj.type;;
 
     if (!messageObj) {
  
