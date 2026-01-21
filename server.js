b4b9631c41c49c3b79e2d28d7fb84dae8e5e8dfe
Maleska <alejandro.matias@hotmail.com>
2026-01-21 14:15:24 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ app.post("/checkin", async (req, res) => {
     }
 
     const ahora = new Date();
-    const horaActual = ahora.toLocaleTimeString('es-MX', { hour: '2-digit', minute: '2-digit' });
+    const horaActual = ahora.toLocaleTimeString('es-MX', { hour: '2-digit', minute: '2-digit', hour12: false });
 
     const cfg = cfgSnap.val();
     const finSemana = esFinDeSemana();
@@ -356,7 +356,7 @@ function fueraDeTolerancia(horaActual, horaBase, tolerancia) {
   console.log("valor hora actual en minutos", (parseInt(h1) * 60 )+ parseInt(m1));
   console.log("valor hora base en minutos + tolerancia", (parseInt(h2) * 60) + parseInt(m2) + parseInt(tolerancia));
 
-  return (parseInt(h1) * 60) + parseInt(m1) > (parseInt(h2) * 60 + parseInt(m2) + parseInt(tolerancia));
+  return (parseInt(h1) * 60) + parseInt(m1) < (parseInt(h2) * 60 + parseInt(m2) + parseInt(tolerancia));
 }
 
 // -----------------------------------------------
